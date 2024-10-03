9dfd4fd7d0f6437764a4949cbee927e10dc34444
Saara <saaraleni@@gmail.com>
2024-10-03 15:16:42 +0300

    smaaall progress

--- push_swap.c
+++ push_swap.c
@@ -6,68 +6,136 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:21:28 by sniemela          #+#    #+#             */
-/*   Updated: 2024/10/02 17:15:32 by sniemela         ###   ########.fr       */
+/*   Updated: 2024/10/03 15:15:25 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_node *a, t_node *b)
+void	rev_rotate(t_stack **stack)
 {
-	int	temp;
-	
-	temp = a->nbr;
-	a->nbr = b->nbr;
-	b->nbr = temp->nbr;
-	ft_printf("sa\n");
-}
-void	rra(t_node **stack_a)
-{
-		
-}
-void	ra(t_node **stack_a)
-{
-	t_node	*temp;
-	t_node	*last;
+	if (stack->top && stack->size > 1)
+	{
+		t_node	*last;
+		t_node	*before_last;
 
-	temp = *stack_a;
-	last = *stack_a;
-	while (last->next)
-		last = last->next;
-	*stack_a = (*stack_a)->next;
-	(*stack_a)-> = NULL;
-	last->next = temp;
-	temp->prev = last;
-	temp->next = NULL;
+		last = stack->top;
+		while (last->next != stack->top)
+		{
+			before_last = last;
+			last = last->next;
+		}
+		stack->top = last;
+		before_last->next = stack->top;
+	}
+}
+
+void	rra(t_stack **stack_a)
+{
+	rev_rotate(stack_a);
+	ft_printf("rra\n");
+}
+
+void	rrb(t_stack **stack_b)
+{
+	rev_rotate(stack_b);
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	rra(stack_a);
+	rrb(stack_b);
+	ft_printf("rrr\n");
+}
+void	rotate(t_stack **stack)
+{
+	if (stack->top && stack->size > 1)
+	{
+		t_node	*second;
+
+		second = stack->top;
+		second = second->next;
+		stack->top = second;
+	}
+}
+
+void	ra(t_stack **stack_a)
+{
+	rotate(stack_a);
 	ft_printf("ra\n");
 }
+void	rb(t_stack **stack_b)
+{
+	rotate(stack_b);
+	ft_printf("rb\n");
+}
 
-void	sort_three(t_node *stack_a)
+void	rr(t_stack **stack_a, t_stack **stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+	ft_printf("rr\n");
+}
+
+void	sb(t_stack **stack_b)
+{
+	swap(stack_b);
+	ft_printf("sb\n");
+}
+
+void	sa(t_stack **stack_a)
+{
+	swap(stack_a);
+	ft_printf("sa\n");
+}
+
+void	ss(t_stack **stack_a, t_stack **stack_b)
+{
+	sa(stack_a);
+	sb(stack_b);
+	ft_printf("ss\n");
+}
+
+void	swap(t_node **stack)
+{
+	if (stack->top && stack->size > 1)
+	t_node	*first;
+	t_node	*second;
+
+	first = stack->top;
+	second = stack->top->next;
+	first->next = second->next;
+	second->next = first;
+	stack->top = second;	
+}
+
+void	sort_three(t_stack *stack_a)
 {
 	int	a;
 	int	b;
 	int	c;
 
-	a = stack_a->nbr;
-	b = stack_a->next->nbr;
-	c = stack_a->next->next->nbr;
+	a = stack_a->top->nbr;
+	b = stack_a->top->next->nbr;
+	c = stack_a->top->next->next->nbr;
 	if (a > b && b > c) // 3 2 1
-		swap(*stack_a,  (*stack_a)->next); // 2 3 1
-		rra(*stack_a); //1 2 3
+		sa(&stack_a); // 2 3 1
+		rra(&stack_a); //1 2 3
 	else if (a > c && c > b) // 3 1 2
 		ra(&stack_a); // 1 2 3
 	else if (b > a && b > c)// 1 3 2
 	{
 		rra(&stack_a); // 2 1 3
-		swap(*stack_a, (*stack_a)->next); // 1 2 3
+		sa(&stack_a); // 1 2 3
 	}
 	else if (a > b && c > a) // 2 1 3
-		swap(*stack_a, (*stack_a)->next);  // 1 2 3
+		sa(&stack_a);  // 1 2 3
 	else if (a > c && b > a) // 2 3 1
 		rra(&stack_a); // 1 2 3
 }
 
-void	add_to_stack(t_node **stack, int nbr)
+void	add_to_stack(t_node *stack, int nbr)
 {
 	t_node	*new_node;
 	t_node	*temp;
@@ -75,24 +143,23 @@ void	add_to_stack(t_node **stack, int nbr)
 	new_node = malloc(sizeof(t_node));
 	if (!new_node)
 		return ;
-	new_node->nbr = nbr
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	if (*stack == NULL)
-		*stack = new_node;
+	new_node->nbr = nbr;
+	if (stack->top == NULL)
+		stack->top = new_node;
 	else
 	{
-		temp = *stack;
-		while (temp->next != NULL)
+		temp = stack->top;
+		while (temp->next != stack->top)
 			temp = temp->next;
-			temp->next = new_node;
-			new_node->prev = temp;
+		temp->next = new_node;
+		new_node->next = stack->top;
 	}
+	stack->size++;
 }
 
 int	main(int ac, char **av)
 {
-	t_node	*stack_a = NULL;
+	t_stack	stack_a = {NULL, 0};
 	int	i;
 	int	num;
 
@@ -103,5 +170,7 @@ int	main(int ac, char **av)
 		add_to_stack(&stack_a, num);
 		i++;
 	}
+	if (stack_a.size == 3)
+		sort_three(&stack_a);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:14:36 by sniemela          #+#    #+#             */
-/*   Updated: 2024/10/02 17:02:39 by sniemela         ###   ########.fr       */
+/*   Updated: 2024/10/03 09:51:55 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@ typedef struct s_node
 	int				nbr;
 	struct s_node	*next;
 } t_node;
+
+typedef struct s_stack
+{
+	t_node			*top;
+	int				size;
+} t_stack;
 
 #endif
