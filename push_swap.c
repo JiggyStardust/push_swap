419e20a5af98f82efd5b27cf19861a9dcd997668
Saara <saaraleni@@gmail.com>
2024-10-23 15:47:15 +0300

    need to modify atoi

--- push_swap.c
+++ push_swap.c
@@ -6,35 +6,11 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:21:28 by sniemela          #+#    #+#             */
-/*   Updated: 2024/10/22 16:00:10 by sniemela         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:11:12 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
-
-void print_stack(t_stack *stack)
-{
-    if (!stack)
-    {
-        ft_printf("Stack is empty\n");
-        return;
-    }
-    t_stack *current = stack;
-    int count = 0;  // Counter to limit printing to 4 elements
-
-    while (current && count < 13)
-    {
-        ft_printf("%d -> ", current->nbr);
-        current = current->next;
-        count++;
-
-        // Check if we have reached the head again to stop
-        if (current == stack)
-            break;
-    }
-	ft_printf("(back to %d)\n", stack->nbr);
-}
 
 int		max_in_stack(t_stack **stack)
 {
@@ -113,31 +89,122 @@ int		stack_size(t_stack *lst)
 	return (nodes);
 }
 
+void	free_2d_arr(char **array)
+{
+	int	i;
+
+	i = 0;
+	if (!array)
+		return ;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	array = NULL;
+}
+
+static int	get_length(char **av, int ac)
+{
+	int	len;
+	int	i;
+
+	i = 1;
+	len = 0;
+	while (i < ac)
+	{
+		len += ft_strlen(av[i]) + 1;
+		i++;
+	}
+	return (len);
+}
+
+static char	**handle_input(char **av, int ac)
+{
+	char	*arg;
+	char	**ret;
+	int		i;
+	int		j;
+	int 	k;
+
+	arg = (char *)malloc(get_length(av, ac) * sizeof(char));
+	if (!arg)
+		return (NULL);
+	i = 1;
+	k = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+			arg[k++] = av[i][j++];
+		if (i < ac - 1)
+			arg[k++] = ' ';
+		i++;
+	}
+	ret = ft_split(arg, ' ');
+	free(arg);
+	return (ret);
+}
+
+static int	mod_atoi(char **str)
+{
+	long	minus;
+	long	nbr;
+
+	nbr = 0;
+	minus = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			minus = 1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		nbr = nbr * 10 + (*str - '0');
+		str++;
+	}
+	if (nbr < 0 && minus == 1)
+		return (0);
+	if (nbr < 0)
+		return (-1);
+	if (minus == 1)
+		nbr = nbr * (-1);
+	return ((int)nbr);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	char	**args;
+	int		i;
+	int		num;
 
-	int	i;
-	int	num;
-
+	if (ac < 2)
+		return (0);
+	args = handle_input(av, ac);
+	if (!args)
+		return (1);
 	stack_a	= NULL;
 	stack_b = NULL;
-	i = 1;
-	while (i < ac)
+	i = 0;
+	while (args[i] != NULL)
 	{
-		num = ft_atoi(av[i]);
+		num = ft_atoi(args[i]);
 		add_to_stack(&stack_a, num);
-//		ft_printf("Added %d to stack.\n", num);
 		i++;
 	}
+	free_2d_arr(args);
 	if (ac <= 5 && !already_sorted(stack_a))
 		sort_small(&stack_a, &stack_b);
-	else
+	else if (!already_sorted(stack_a))
 		sort_big(&stack_a, &stack_b);
-	 ft_printf("In the end: \nstack_a: ");
-	 print_stack(stack_a);
-	 ft_printf("stack_b: ");
-	 print_stack(stack_b);
+	while (stack_a)
+		free_node(&stack_a);
 	return (0);
 }
+
